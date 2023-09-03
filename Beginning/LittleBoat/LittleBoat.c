b9b5c118079b9ad3e75357e1b13f0c08f2e7e19d
EzDev <105008901+EzArmy@users.noreply.github.com>
2023-09-03 03:51:02 -0300

    Create LittleBoat.c

---
+++ Beginning/LittleBoat/LittleBoat.c
@@ -0,0 +1,14 @@
+int main()
+{
+    printf("          /| \n");
+    printf("         / | \n");
+    printf("        /  | \n");
+    printf("       /   | \n");
+    printf("      /    | \n");
+    printf("     /     | \n");
+    printf("    /______| \n");
+    printf("___________|________ \n");
+    printf("\\   S.S. LOL       / \n");
+    printf(" \\________________/   \n");
+    return 0;
+}
